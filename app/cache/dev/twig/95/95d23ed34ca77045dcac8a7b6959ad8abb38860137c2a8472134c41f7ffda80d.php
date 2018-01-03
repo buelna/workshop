a529ad1bfc98b8b3caea9ad399b48d96c8a93039
Buelna <andres.buelna.b@gmail.com>
2018-01-03 15:57:05 -0800

    program

--- app/cache/dev/twig/95/95d23ed34ca77045dcac8a7b6959ad8abb38860137c2a8472134c41f7ffda80d.php
+++ app/cache/dev/twig/95/95d23ed34ca77045dcac8a7b6959ad8abb38860137c2a8472134c41f7ffda80d.php
@@ -22,50 +22,46 @@ class __TwigTemplate_1b3226323b55b3ba4ad97e09c22c4dc711eb9b1e27b05eb880d673632f7
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_da54ca41735a506049b2c28a5526ab7e6af348f1c6dd9dbdbffcf250597adfd2 = $this->env->getExtension("native_profiler");
-        $__internal_da54ca41735a506049b2c28a5526ab7e6af348f1c6dd9dbdbffcf250597adfd2->enter($__internal_da54ca41735a506049b2c28a5526ab7e6af348f1c6dd9dbdbffcf250597adfd2_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "AppBundle:Default:programa.html.twig"));
+        $__internal_5e0a5d7e7299f6492b8ea9cbb626cb920c65ab8f450d0462cbad3db0a5e10530 = $this->env->getExtension("native_profiler");
+        $__internal_5e0a5d7e7299f6492b8ea9cbb626cb920c65ab8f450d0462cbad3db0a5e10530->enter($__internal_5e0a5d7e7299f6492b8ea9cbb626cb920c65ab8f450d0462cbad3db0a5e10530_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "AppBundle:Default:programa.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_da54ca41735a506049b2c28a5526ab7e6af348f1c6dd9dbdbffcf250597adfd2->leave($__internal_da54ca41735a506049b2c28a5526ab7e6af348f1c6dd9dbdbffcf250597adfd2_prof);
+        $__internal_5e0a5d7e7299f6492b8ea9cbb626cb920c65ab8f450d0462cbad3db0a5e10530->leave($__internal_5e0a5d7e7299f6492b8ea9cbb626cb920c65ab8f450d0462cbad3db0a5e10530_prof);
 
     }
 
     // line 2
     public function block_content($context, array $blocks = array())
     {
-        $__internal_dc438e0ccc1bad2734d01e85dea154024fd3c216425841233a8e55b87efd6a67 = $this->env->getExtension("native_profiler");
-        $__internal_dc438e0ccc1bad2734d01e85dea154024fd3c216425841233a8e55b87efd6a67->enter($__internal_dc438e0ccc1bad2734d01e85dea154024fd3c216425841233a8e55b87efd6a67_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
+        $__internal_dbcf929872c4146c5ef9b94656a3d155eb7ddb2211680ec6d88d38c009169498 = $this->env->getExtension("native_profiler");
+        $__internal_dbcf929872c4146c5ef9b94656a3d155eb7ddb2211680ec6d88d38c009169498->enter($__internal_dbcf929872c4146c5ef9b94656a3d155eb7ddb2211680ec6d88d38c009169498_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
 
         // line 3
         echo "<ul class=\"pricing-table\">
-  <li class=\"title\"><strong>To be announced...</strong></li>
-</ul>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<!--
-<ul class=\"pricing-table\">
-  <li class=\"title\"><strong>Technical Program<br>Saturday, January 9, 2016</strong></li>
+  <li class=\"title\"><strong>Technical Program<br>Friday, January 12, 2018</strong></li>
 </ul>
 <ul class=\"pricing-table\">
-  <li class=\"title\"><strong>9:00 – 10:00: AALEH - Ambient Assisted Living and eHealth (1)</strong><br>
+  <li class=\"title\"><strong>8:30 – 10:00: AALEH - Ambient Assisted Living and eHealth (1)</strong><br>
     <strong>Session chair: Prof. Arnoldo Diaz-Ramirez</strong><br>
-    <strong>Room: Sunset 1</strong></li>
+    <strong>Room: VIRGINIA CITY</strong></li>
    
   <li class=\"bullet-item\">
      <font size=\"3\">
-      <strong>User Interface Design for Privacy Awareness in eHealth Technologies</strong><br>
-      Isabel Wagner<sup>1</sup>; Ying He<sup>1</sup>; Duska Rosenberg<sup>1</sup>; Helge Janicke<sup>1</sup> (<sup>1</sup>De Montfort University, UK)
+      <strong>RehabPartner: Motion Tracking Assistant Using a Novel Complementary Feedback Filter</strong><br>
+      Shao Loong Lim (Nanyang Technological University Singapore, Singapore); Seanglidet Yean, Bu Sung Lee and Chai Kiat (Nanyang Technological University Singapore, Singapore)
       </font>
   </li>
   <li class=\"bullet-item\">
     <font size=\"3\">
-      <strong>An Improved Joint Power Harvesting and Communication Technology for Smartphone Centric Ubiquitous Sensing Applications</strong><br>
-      Hong Nie<sup>1</sup>; Ranjana Joshi<sup>1</sup>; Ziyuan Li<sup>1</sup> (<sup>1</sup>University of Northern Iowa, USA)
+      <strong>Development and evaluation of a Healthy Coping voice interface application using the Google Home for elderly patients with Type 2 Diabetes</strong><br>
+      Amy Cheng (Auburn University, USA); Vaishnavi Raghavaraju (University of Cincinnati, USA); Jayanth Kanugo (University of Missouri, USA); Yohanes Handrianto and Yi Shang (University of Missouri, USA)
+    </font>
+  </li>
+  <li class=\"bullet-item\">
+    <font size=\"3\">
+      <strong>Tacit Computing and its Application for Open IoT Era</strong><br>
+      Misao Kataoka (NTT, Japan); Naoto Hoshikawa (NTT Network Service Systems Laboratories & NTT, Japan); Hirofumi Noguchi and Tatsuya Demizu (Nippon Telegraph and Telephone Corporation, Japan); Yoji Yamato (NTT Corporation, Japan)
     </font>
   </li>
 </ul>
@@ -73,52 +69,46 @@ class __TwigTemplate_1b3226323b55b3ba4ad97e09c22c4dc711eb9b1e27b05eb880d673632f7
   <li class=\"title\"><strong>10:00 – 10:30: Coffee Break and Networking</strong></li>
 </ul>
 <ul class=\"pricing-table\">
-  <li class=\"title\"><strong>10:30 – 13:00: AALEH - Ambient Assisted Living and eHealth (2)</strong><br>
+  <li class=\"title\"><strong>10:30 – 12:30: AALEH - Ambient Assisted Living and eHealth (2)</strong><br>
     <strong>Session chair: Prof. Arnoldo Diaz-Ramirez</strong><br>
-    <strong>Room: Sunset 1</strong></li>
+    <strong>Room: VIRGINIA CITY</strong></li>
   <li class=\"bullet-item\">
     <font size=\"3\">
-      <strong>Modeling the Long Term Fading Behaviour in Residential Wireless Sensor Networks</strong><br>
-      Falk-Moritz Schaefer<sup>1</sup> (<sup>1</sup>Accenture Technology, Accenture GmbH, Düsseldorf, Germany); Ruediger Kays<sup>2</sup> (<sup>2</sup>TU Dortmund University, Germany)  
+      <strong>A Fuzzy Control-based Approach for the Selection of Health Interventions</strong><br>
+      Juan Calderon (2209 Fitness club way apt 203, USA); Luis G Jaimes (Florida Polytechnic University, USA)
     </font>
   </li>
   <li class=\"bullet-item\">
     <font size=\"3\">
-      <strong>PAT: A Power-Aware Decision Tree Algorithm for Mobile Activity Recognition</strong><br>
-      Luis G Jaimes<sup>1</sup> (<sup>1</sup>Bethune-Cookman University, USA); Yueng Delahoz<sup>2</sup>; Christopher Eggert<sup>2</sup>; Idalides Vergara-Laurens<sup>2</sup> (<sup>2</sup>University of South Florida, USA)
+      <strong>Crowd-based positioning of UAVs as Access Points</strong><br>
+      Dorin Rautu and Riadh Dhaou (IRIT/ENSEEIHT, University of Toulouse, France); Emmanuel Chaput (Irit-Enseeiht, France)
     </font>
   </li>
   <li class=\"bullet-item\">
     <font size=\"3\">
-      <strong>Encoding Distortion Modeling For DWT-Based Wireless EEG Monitoring System</strong><br>
-      Alaa Awad Abdellatif<sup>1</sup>; Medhat Hamdy<sup>2</sup> (<sup>2</sup>Carleton University, Canada); Amr Mohamed<sup>1</sup> (<sup>1</sup>Qatar University, Qatar)
-    </font>
-  </li>
-  <li class=\"bullet-item\">
-    <font size=\"3\">
-      <strong>Hybrid Rapid Response Routing Approach for Delay-Sensitive Data in Hospital Body Area Sensor Network</strong><br>
-      Tariq Umer<sup>1</sup>; Muhammad Amjad<sup>1</sup>; Muhammad Khalil Afzal<sup>1</sup> (<sup>1</sup>COMSATS Institute of Information Technology, Pakistan); Muhammad Aslam<sup>2</sup> (<sup>2</sup>School of Computer Science and Technology Dalian University of Technology, China)
+      <strong>Utilization-based Parking Space Suggestion in Smart City</strong><br>
+      Chia-Ying Lin, Yi-Lung Lu, Meng-Hsun Tsai and Hui-Ling Chang (National Cheng Kung University, Taiwan)up>2</sup>Carleton University, Canada); Amr Mohamed<sup>1</sup> (<sup>1</sup>Qatar University, Qatar)
     </font>
   </li>
 </ul>
-</font>-->
+</font>
 ";
         
-        $__internal_dc438e0ccc1bad2734d01e85dea154024fd3c216425841233a8e55b87efd6a67->leave($__internal_dc438e0ccc1bad2734d01e85dea154024fd3c216425841233a8e55b87efd6a67_prof);
+        $__internal_dbcf929872c4146c5ef9b94656a3d155eb7ddb2211680ec6d88d38c009169498->leave($__internal_dbcf929872c4146c5ef9b94656a3d155eb7ddb2211680ec6d88d38c009169498_prof);
 
     }
 
-    // line 68
+    // line 58
     public function block_cols($context, array $blocks = array())
     {
-        $__internal_09fed3b1a19597168d025a112b54b78ee333cef032e41d12600aba0cbf856938 = $this->env->getExtension("native_profiler");
-        $__internal_09fed3b1a19597168d025a112b54b78ee333cef032e41d12600aba0cbf856938->enter($__internal_09fed3b1a19597168d025a112b54b78ee333cef032e41d12600aba0cbf856938_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "cols"));
+        $__internal_4bc0c8b87bbecefbd23cdc9f9f1f6e165eab10d7a4369323f7fc48082c22eccb = $this->env->getExtension("native_profiler");
+        $__internal_4bc0c8b87bbecefbd23cdc9f9f1f6e165eab10d7a4369323f7fc48082c22eccb->enter($__internal_4bc0c8b87bbecefbd23cdc9f9f1f6e165eab10d7a4369323f7fc48082c22eccb_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "cols"));
 
-        // line 69
+        // line 59
         echo "<div class=\"large-12 columns\">
 ";
         
-        $__internal_09fed3b1a19597168d025a112b54b78ee333cef032e41d12600aba0cbf856938->leave($__internal_09fed3b1a19597168d025a112b54b78ee333cef032e41d12600aba0cbf856938_prof);
+        $__internal_4bc0c8b87bbecefbd23cdc9f9f1f6e165eab10d7a4369323f7fc48082c22eccb->leave($__internal_4bc0c8b87bbecefbd23cdc9f9f1f6e165eab10d7a4369323f7fc48082c22eccb_prof);
 
     }
 
@@ -134,39 +124,35 @@ class __TwigTemplate_1b3226323b55b3ba4ad97e09c22c4dc711eb9b1e27b05eb880d673632f7
 
     public function getDebugInfo()
     {
-        return array (  118 => 69,  112 => 68,  41 => 3,  35 => 2,  11 => 1,);
+        return array (  108 => 59,  102 => 58,  41 => 3,  35 => 2,  11 => 1,);
     }
 }
 /* {% extends '::base.html.twig' %}*/
 /* {% block content %}*/
 /* <ul class="pricing-table">*/
-/*   <li class="title"><strong>To be announced...</strong></li>*/
-/* </ul>*/
-/* <br>*/
-/* <br>*/
-/* <br>*/
-/* <br>*/
-/* <br>*/
-/* <br>*/
-/* <!--*/
-/* <ul class="pricing-table">*/
-/*   <li class="title"><strong>Technical Program<br>Saturday, January 9, 2016</strong></li>*/
+/*   <li class="title"><strong>Technical Program<br>Friday, January 12, 2018</strong></li>*/
 /* </ul>*/
 /* <ul class="pricing-table">*/
-/*   <li class="title"><strong>9:00 – 10:00: AALEH - Ambient Assisted Living and eHealth (1)</strong><br>*/
+/*   <li class="title"><strong>8:30 – 10:00: AALEH - Ambient Assisted Living and eHealth (1)</strong><br>*/
 /*     <strong>Session chair: Prof. Arnoldo Diaz-Ramirez</strong><br>*/
-/*     <strong>Room: Sunset 1</strong></li>*/
+/*     <strong>Room: VIRGINIA CITY</strong></li>*/
 /*    */
 /*   <li class="bullet-item">*/
 /*      <font size="3">*/
-/*       <strong>User Interface Design for Privacy Awareness in eHealth Technologies</strong><br>*/
-/*       Isabel Wagner<sup>1</sup>; Ying He<sup>1</sup>; Duska Rosenberg<sup>1</sup>; Helge Janicke<sup>1</sup> (<sup>1</sup>De Montfort University, UK)*/
+/*       <strong>RehabPartner: Motion Tracking Assistant Using a Novel Complementary Feedback Filter</strong><br>*/
+/*       Shao Loong Lim (Nanyang Technological University Singapore, Singapore); Seanglidet Yean, Bu Sung Lee and Chai Kiat (Nanyang Technological University Singapore, Singapore)*/
 /*       </font>*/
 /*   </li>*/
 /*   <li class="bullet-item">*/
 /*     <font size="3">*/
-/*       <strong>An Improved Joint Power Harvesting and Communication Technology for Smartphone Centric Ubiquitous Sensing Applications</strong><br>*/
-/*       Hong Nie<sup>1</sup>; Ranjana Joshi<sup>1</sup>; Ziyuan Li<sup>1</sup> (<sup>1</sup>University of Northern Iowa, USA)*/
+/*       <strong>Development and evaluation of a Healthy Coping voice interface application using the Google Home for elderly patients with Type 2 Diabetes</strong><br>*/
+/*       Amy Cheng (Auburn University, USA); Vaishnavi Raghavaraju (University of Cincinnati, USA); Jayanth Kanugo (University of Missouri, USA); Yohanes Handrianto and Yi Shang (University of Missouri, USA)*/
+/*     </font>*/
+/*   </li>*/
+/*   <li class="bullet-item">*/
+/*     <font size="3">*/
+/*       <strong>Tacit Computing and its Application for Open IoT Era</strong><br>*/
+/*       Misao Kataoka (NTT, Japan); Naoto Hoshikawa (NTT Network Service Systems Laboratories & NTT, Japan); Hirofumi Noguchi and Tatsuya Demizu (Nippon Telegraph and Telephone Corporation, Japan); Yoji Yamato (NTT Corporation, Japan)*/
 /*     </font>*/
 /*   </li>*/
 /* </ul>*/
@@ -174,35 +160,29 @@ class __TwigTemplate_1b3226323b55b3ba4ad97e09c22c4dc711eb9b1e27b05eb880d673632f7
 /*   <li class="title"><strong>10:00 – 10:30: Coffee Break and Networking</strong></li>*/
 /* </ul>*/
 /* <ul class="pricing-table">*/
-/*   <li class="title"><strong>10:30 – 13:00: AALEH - Ambient Assisted Living and eHealth (2)</strong><br>*/
+/*   <li class="title"><strong>10:30 – 12:30: AALEH - Ambient Assisted Living and eHealth (2)</strong><br>*/
 /*     <strong>Session chair: Prof. Arnoldo Diaz-Ramirez</strong><br>*/
-/*     <strong>Room: Sunset 1</strong></li>*/
+/*     <strong>Room: VIRGINIA CITY</strong></li>*/
 /*   <li class="bullet-item">*/
 /*     <font size="3">*/
-/*       <strong>Modeling the Long Term Fading Behaviour in Residential Wireless Sensor Networks</strong><br>*/
-/*       Falk-Moritz Schaefer<sup>1</sup> (<sup>1</sup>Accenture Technology, Accenture GmbH, Düsseldorf, Germany); Ruediger Kays<sup>2</sup> (<sup>2</sup>TU Dortmund University, Germany)  */
+/*       <strong>A Fuzzy Control-based Approach for the Selection of Health Interventions</strong><br>*/
+/*       Juan Calderon (2209 Fitness club way apt 203, USA); Luis G Jaimes (Florida Polytechnic University, USA)*/
 /*     </font>*/
 /*   </li>*/
 /*   <li class="bullet-item">*/
 /*     <font size="3">*/
-/*       <strong>PAT: A Power-Aware Decision Tree Algorithm for Mobile Activity Recognition</strong><br>*/
-/*       Luis G Jaimes<sup>1</sup> (<sup>1</sup>Bethune-Cookman University, USA); Yueng Delahoz<sup>2</sup>; Christopher Eggert<sup>2</sup>; Idalides Vergara-Laurens<sup>2</sup> (<sup>2</sup>University of South Florida, USA)*/
+/*       <strong>Crowd-based positioning of UAVs as Access Points</strong><br>*/
+/*       Dorin Rautu and Riadh Dhaou (IRIT/ENSEEIHT, University of Toulouse, France); Emmanuel Chaput (Irit-Enseeiht, France)*/
 /*     </font>*/
 /*   </li>*/
 /*   <li class="bullet-item">*/
 /*     <font size="3">*/
-/*       <strong>Encoding Distortion Modeling For DWT-Based Wireless EEG Monitoring System</strong><br>*/
-/*       Alaa Awad Abdellatif<sup>1</sup>; Medhat Hamdy<sup>2</sup> (<sup>2</sup>Carleton University, Canada); Amr Mohamed<sup>1</sup> (<sup>1</sup>Qatar University, Qatar)*/
-/*     </font>*/
-/*   </li>*/
-/*   <li class="bullet-item">*/
-/*     <font size="3">*/
-/*       <strong>Hybrid Rapid Response Routing Approach for Delay-Sensitive Data in Hospital Body Area Sensor Network</strong><br>*/
-/*       Tariq Umer<sup>1</sup>; Muhammad Amjad<sup>1</sup>; Muhammad Khalil Afzal<sup>1</sup> (<sup>1</sup>COMSATS Institute of Information Technology, Pakistan); Muhammad Aslam<sup>2</sup> (<sup>2</sup>School of Computer Science and Technology Dalian University of Technology, China)*/
+/*       <strong>Utilization-based Parking Space Suggestion in Smart City</strong><br>*/
+/*       Chia-Ying Lin, Yi-Lung Lu, Meng-Hsun Tsai and Hui-Ling Chang (National Cheng Kung University, Taiwan)up>2</sup>Carleton University, Canada); Amr Mohamed<sup>1</sup> (<sup>1</sup>Qatar University, Qatar)*/
 /*     </font>*/
 /*   </li>*/
 /* </ul>*/
-/* </font>-->*/
+/* </font>*/
 /* {% endblock %}*/
 /* {% block cols %}*/
 /* <div class="large-12 columns">*/
